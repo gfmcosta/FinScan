0619a0edcec861887901c3aa6f68f5161addb6c8
Gonçalo Costa <goncalo.costa@bravantic.com>
2026-04-27 23:44:52 +0100

    feat: Added Base Home Screen

--- app/src/main/java/pt/ipt/dama2026/finscan/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama2026/finscan/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import pt.ipt.dama2026.finscan.ui.screens.SplashScreen
 import pt.ipt.dama2026.finscan.ui.screens.MainScreen
