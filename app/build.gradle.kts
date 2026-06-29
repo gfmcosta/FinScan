ea6f41d8ba55d4c85b213523bfe997612d751291
Gonçalo Costa <goncalofilipecosta@outlook.pt>
2026-06-30 00:47:48 +0100

    update deployment version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "pt.ipt.dama.FinScan"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
